63c33544db20427c5e205cfc1d0f382e8d760ff1
arucadev <arucadev@gmail.com>
2025-05-09 10:01:41 +0200

    Add test

--- src/test/PokeApiClientTest.java
+++ src/test/PokeApiClientTest.java
@@ -1,4 +1,26 @@
 package test;
 
+import api.PokeApiClient;
+
 public class PokeApiClientTest {
+    public static void main(String[] args) {
+        // Test the PokeApiClient class
+        PokeApiClient client = new PokeApiClient();
+
+        // Test fetching a Pokemon
+        Pokemon pokemon = client.getPokemon("pikachu");
+        System.out.println("Pokemon: " + pokemon);
+
+        // Test fetching a Type
+        Type type = client.getType("electric");
+        System.out.println("Type: " + type);
+
+        // Test fetching a Region
+        Region region = client.getRegion("kanto");
+        System.out.println("Region: " + region);
+
+        // Test fetching a Move
+        Move move = client.getMove("thunderbolt");
+        System.out.println("Move: " + move);
+    }
 }
