61d7397525edcfa91e33bcf6810aefe61e778c1e
arucadev <arucadev@gmail.com>
2025-05-12 08:57:56 +0200

    Update Test

--- src/test/PokeApiClientTest.java
+++ src/test/PokeApiClientTest.java
@@ -4,6 +4,7 @@ import api.PokeApiClient;
 
 public class PokeApiClientTest {
     public static void main(String[] args) {
+        /*
         // Test the PokeApiClient class
         PokeApiClient client = new PokeApiClient();
 
@@ -22,5 +23,6 @@ public class PokeApiClientTest {
         // Test fetching a Move
         Move move = client.getMove("thunderbolt");
         System.out.println("Move: " + move);
+        */
     }
 }
