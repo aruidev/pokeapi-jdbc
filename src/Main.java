1eed0b073281b24e0f69be68760aced236e183ab
arui <arucadev@gmail.com>
2025-05-13 09:12:58 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,43 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Scanner;
 
-import model.SQLite.SQLiteLocationDAO;
-import model.constructor.Location;
+import controller.HandleMenu;
+import model.SQLite.*;
+import view.*;
+import model.dbconnection;
 
+import static model.dbconnection.closeCon;
 import static model.dbconnection.openCon;
 
 public class Main {
     public static void main(String[] args) {
+        try (Connection connection = openCon("jdbc:sqlite:db/pokeapiDB.db");
+             Scanner scan = new Scanner(System.in)) {
 
+            SQLiteAbilityDAO abilityDAO = new SQLiteAbilityDAO();
+            SQLiteGenerationDAO generationDAO = new SQLiteGenerationDAO();
+            SQLiteLocationDAO locationDAO = new SQLiteLocationDAO();
+            SQLiteMoveDAO moveDAO = new SQLiteMoveDAO();
+            SQLitePokemonDAO pokemonDAO = new SQLitePokemonDAO();
+            SQLiteRegionDAO regionDAO = new SQLiteRegionDAO();
+            SQLiteTypeDAO typeDAO = new SQLiteTypeDAO();
+
+            abilityDAO.setConnection(connection);
+            generationDAO.setConnection(connection);
+            locationDAO.setConnection(connection);
+            moveDAO.setConnection(connection);
+            pokemonDAO.setConnection(connection);
+            regionDAO.setConnection(connection);
+            typeDAO.setConnection(connection);
+
+            HandleMenu menu = new HandleMenu();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Programa finalitzat.");
+            System.out.println("Connexió amb la base de dades finalitzada.");
+        }
     }
 }
