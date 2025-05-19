d20e38c3553f9ab14c71c95a3c3b0c8fc68416d7
arucadev <arucadev@gmail.com>
2025-05-19 12:09:13 +0200

    Translate error messages to Catalan

--- src/model/SQLite/SQLitePokemonDAO.java
+++ src/model/SQLite/SQLitePokemonDAO.java
@@ -16,22 +16,22 @@ public class SQLitePokemonDAO implements DAO<Pokemon, Integer> {
     @Override
     public void insertTable(Pokemon pokemon) {
         if (pokemon.getNom() == null) {
-            throw new InvalidDataException("No se puede insertar un Pokémon nulo o sin nombre");
+            throw new InvalidDataException("No es pot insertar un pokemon nul o sense nom");
         }
         if (pokemon.getId_pokemon() <= 0) {
-            throw new InvalidDataException("No se puede insertar un Pokémon con ID menor o igual a 0");
+            throw new InvalidDataException("No es pot insertar un pokemon amb ID menor o igual a 0");
         }
         if (pokemon.getPes() <= 0) {
-            throw new InvalidDataException("No se puede insertar un Pokémon con peso menor o igual a 0");
+            throw new InvalidDataException("No es pot insertar un pokemon amb pes menor o igual a 0");
         }
         if (pokemon.getAltura() <= 0) {
-            throw new InvalidDataException("No se puede insertar un Pokémon con altura menor o igual a 0");
+            throw new InvalidDataException("No es pot insertar un pokemon amb altura menor o igual a 0");
         }
         if (pokemon.getId_generacio() <= 0) {
-            throw new InvalidDataException("No se puede insertar un Pokémon con ID de generación menor o igual a 0");
+            throw new InvalidDataException("No es pot insertar un pokemon amb ID de generació menor o igual a 0");
         }
         if (pokemon.getId_tipus() <= 0) {
-            throw new InvalidDataException("No se puede insertar un Pokémon con ID de tipo menor o igual a 0");
+            throw new InvalidDataException("No es pot insertar un pokemon amb ID de tipus menor o igual a 0");
         }
         String sql = "INSERT INTO pokemons (id_pokemon, nom, pes, altura, id_generacio, id_tipus) VALUES (?, ?, ?, ?, ?, ?)";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
@@ -44,9 +44,9 @@ public class SQLitePokemonDAO implements DAO<Pokemon, Integer> {
             stmt.executeUpdate();
         } catch (SQLException e) {
             if (e.getMessage().contains("UNIQUE constraint failed")) {
-                throw new DuplicateEntryException("Ya existe un Pokémon con ID " + pokemon.getId_pokemon());
+                throw new DuplicateEntryException("Ja existeix un pokemon amb ID " + pokemon.getId_pokemon());
             }
-            throw new DataAccessException("Error during database operation", e);
+            throw new DataAccessException("Error durant la operació a la DB.", e);
         }
     }
 
@@ -67,29 +67,29 @@ public class SQLitePokemonDAO implements DAO<Pokemon, Integer> {
                         rs.getInt("id_generacio"),
                         rs.getInt("id_tipus"));
             } else {
-                throw new PropertyNotFound("No existe el Pokémon con ID " + id);
+                throw new PropertyNotFound("No existeix el pokemon amb la ID " + id);
             }
         } catch (PropertyNotFound | SQLException e) {
-            System.err.println("Error al leer el Pokémon: " + e.getMessage());
+            System.err.println("Error al llegir el pokemon: " + e.getMessage());
         }
     }
 
     @Override
     public void updateTable(Pokemon pokemon) {
         if (pokemon.getNom() == null) {
-            throw new InvalidDataException("No se puede actualizar un Pokémon nulo o sin nombre");
+            throw new InvalidDataException("No es pot actualitzar un pokemon nul o sense nom");
         }
         if (pokemon.getPes() <= 0) {
-            throw new InvalidDataException("No se puede insertar un Pokémon con peso menor o igual a 0");
+            throw new InvalidDataException("No es pot insertar un pokemon amb pes menor o igual a 0");
         }
         if (pokemon.getAltura() <= 0) {
-            throw new InvalidDataException("No se puede insertar un Pokémon con altura menor o igual a 0");
+            throw new InvalidDataException("No es pot actualitzar un pokemon amb altura menor o igual a 0");
         }
         if (pokemon.getId_generacio() <= 0) {
-            throw new InvalidDataException("No se puede insertar un Pokémon con ID de generación menor o igual a 0");
+            throw new InvalidDataException("No es pot insertar un pokemon amb ID de generació menor o igual a 0");
         }
         if (pokemon.getId_tipus() <= 0) {
-            throw new InvalidDataException("No se puede insertar un Pokémon con ID de tipo menor o igual a 0");
+            throw new InvalidDataException("No es pot actualitzar un pokemon amb ID de tipus menor o igual a 0");
         }
         String sql = "UPDATE pokemons SET nom = ?, pes = ?, altura = ?, id_generacio = ?, id_tipus = ? WHERE id_pokemon = ?";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
@@ -101,15 +101,15 @@ public class SQLitePokemonDAO implements DAO<Pokemon, Integer> {
             stmt.setInt(6, pokemon.getId_pokemon());
             stmt.executeUpdate();
             if (stmt.getUpdateCount() == -1) {
-                throw new PropertyNotFound("No existe el Pokémon con ID " + pokemon.getId_pokemon());
+                throw new PropertyNotFound("No existeix el pokemon amb ID " + pokemon.getId_pokemon());
             }
         } catch (PropertyNotFound e) {
-            System.err.println("Error al actualizar el Pokémon: " + e.getMessage());
+            System.err.println("Error al actualitzar el pokemon: " + e.getMessage());
         } catch (SQLException e) {
             if (e.getMessage().contains("UNIQUE constraint failed")) {
-                throw new DuplicateEntryException("Ya existe un Pokémon con ID " + pokemon.getId_pokemon());
+                throw new DuplicateEntryException("Ja existeix un pokemon amb ID " + pokemon.getId_pokemon());
             }
-            throw new DataAccessException("Error during database operation", e);
+            throw new DataAccessException("Error durant la operació a la DB.", e);
         }
     }
 
@@ -120,15 +120,15 @@ public class SQLitePokemonDAO implements DAO<Pokemon, Integer> {
             stmt.setInt(1, id);
             stmt.executeUpdate();
             if (stmt.getUpdateCount() == -1) {
-                throw new PropertyNotFound("No existe el Pokémon con ID " + id);
+                throw new PropertyNotFound("No existeix el pokemon amb la ID " + id);
             }
         } catch (PropertyNotFound e) {
-            System.err.println("Error al eliminar el Pokémon: " + e.getMessage());
+            System.err.println("Error al eliminar el pokemon: " + e.getMessage());
         } catch (SQLException e) {
             if (e.getMessage().contains("FOREIGN KEY constraint failed")) {
-                throw new ForeignKeyConstraintException("No se puede eliminar el Pokémon con ID " + id + " porque está referenciado por otra tabla.");
+                throw new ForeignKeyConstraintException("No es pot eliminar el pokemon amb la ID " + id + " perque s'està referenciant una altre taula.");
             }
-            throw new DataAccessException("Error during database operation", e);
+            throw new DataAccessException("Error durant la operació a la DB.", e);
         }
     }
 
@@ -137,8 +137,8 @@ public class SQLitePokemonDAO implements DAO<Pokemon, Integer> {
         String sql = "SELECT * FROM pokemons";
         try (PreparedStatement stmt = connection.prepareStatement(sql);
              ResultSet rs = stmt.executeQuery()) {
-            if (!rs.isBeforeFirst()) { // Check if the result set is empty
-                throw new EmptyResultSetException("Ningún Pokémon encontrado en la base de datos.");
+            if (!rs.isBeforeFirst()) { // Si el resultat està buit
+                throw new EmptyResultSetException("Ningún pokemon trobat a la DB.");
             }
             System.out.printf("%-5s %-20s %-10s %-10s %-15s %-10s%n", "ID", "Nom", "Pes", "Altura", "Generacio", "Tipus");
             System.out.println("-------------------------------------------------------------");
@@ -154,7 +154,7 @@ public class SQLitePokemonDAO implements DAO<Pokemon, Integer> {
         } catch (EmptyResultSetException e) {
             System.err.println(e.getMessage());
         } catch (SQLException e) {
-            throw new DataAccessException("Error during database operation", e);
+            throw new DataAccessException("Error durant la operació a la DB", e);
         }
     }
 }
